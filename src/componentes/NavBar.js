18ff29f776c178d2051de560d68d57c927513a38
Ramiro Cornejo <ramiro.s.cornejo@gmail.com>
2022-03-31 19:42:16 -0300

    Creacion App

---
+++ src/componentes/NavBar.js
@@ -0,0 +1,12 @@
+
+
+export const NavBar = () => {
+    return (
+        <>
+        <header>
+            <h1>Portfolio Ramiro Cornejo</h1>
+            <h2>Web en proceso</h2>
+        </header>
+        </>
+    )
+}
